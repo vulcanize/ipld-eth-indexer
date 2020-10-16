54c8a9dc7eae2ccb214bcedff692cb04992a3e5b
Ilnur Galiev <ilnur.a.g@gmail.com>
2020-10-16 15:08:12 +0300

    add observation of time metric

--- pkg/prom/prom.go
+++ pkg/prom/prom.go
@@ -1,21 +1,35 @@
 package prom
 
 import (
+	"time"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
+const statsSubsystem = "stats"
+
 var (
-	metrics      bool
+	metrics bool
+
 	receipts     prometheus.Counter
 	transactions prometheus.Counter
 	blocks       prometheus.Counter
+
+	tPayloadDecode           prometheus.Histogram // payload decoding time: 58.173µs
+	tFreePostgres            prometheus.Histogram // time spent waiting for free postgres tx: 94.511µs
+	tPostgresCommit          prometheus.Histogram // postgres transaction commit duration: 1.509643ms
+	tHeaderProcessing        prometheus.Histogram // header processing time: 1.057721ms
+	tUncleProcessing         prometheus.Histogram // uncle processing time: 140ns
+	tTxAndRecProcessing      prometheus.Histogram // tx and receipt processing time: 1.749µs
+	tStateAndStoreProcessing prometheus.Histogram // state and storage processing time: 4.737994ms
 )
 
 // Init module initialization
 func Init() {
 	metrics = true
+
 	blocks = promauto.NewCounter(prometheus.CounterOpts{
 		Namespace: namespace,
 		Name:      "blocks",
@@ -30,6 +44,49 @@ func Init() {
 		Namespace: namespace,
 		Name:      "receipts",
 		Help:      "The total number of processed receipts",
+	})
+
+	tPayloadDecode = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: statsSubsystem,
+		Name:      "t_payload_decode",
+		Help:      "Payload decoding time",
+	})
+	tFreePostgres = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: statsSubsystem,
+		Name:      "t_free_postgres",
+		Help:      "Time spent waiting for free postgres tx",
+	})
+	tPostgresCommit = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: statsSubsystem,
+		Name:      "t_postgres_commit",
+		Help:      "Postgres transaction commit duration",
+	})
+	tHeaderProcessing = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: statsSubsystem,
+		Name:      "t_header_rocessing",
+		Help:      "Header processing time",
+	})
+	tUncleProcessing = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: statsSubsystem,
+		Name:      "t_uncle_processing",
+		Help:      "Uncle processing time",
+	})
+	tTxAndRecProcessing = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: statsSubsystem,
+		Name:      "t_tx_receipt_processing",
+		Help:      "Tx and receipt processing time",
+	})
+	tStateAndStoreProcessing = promauto.NewHistogram(prometheus.HistogramOpts{
+		Namespace: namespace,
+		Subsystem: statsSubsystem,
+		Name:      "t_state_store_processing",
+		Help:      "State and storage processing time",
 	})
 }
 
@@ -58,5 +115,29 @@ func TransactionInc() {
 func ReceiptInc() {
 	if metrics {
 		receipts.Inc()
+	}
+}
+
+// SetTimeMetric time metric observation
+func SetTimeMetric(name string, t time.Duration) {
+	if !metrics {
+		return
+	}
+	tAsF64 := float64(t.Microseconds())
+	switch name {
+	case "t_payload_decode":
+		tPayloadDecode.Observe(tAsF64)
+	case "t_free_postgres":
+		tFreePostgres.Observe(tAsF64)
+	case "t_postgres_commit":
+		tPostgresCommit.Observe(tAsF64)
+	case "t_header_rocessing":
+		tHeaderProcessing.Observe(tAsF64)
+	case "t_uncle_processing":
+		tUncleProcessing.Observe(tAsF64)
+	case "t_tx_receipt_processing":
+		tTxAndRecProcessing.Observe(tAsF64)
+	case "t_state_store_processing":
+		tStateAndStoreProcessing.Observe(tAsF64)
 	}
 }
