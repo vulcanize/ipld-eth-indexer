3eb85f67052e4024840d36069fbd0fa46a4b8c4c
ramil <ramilexe@gmail.com>
2021-02-19 15:04:26 +0300

    bump version

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ import "fmt"
 
 const (
 	Major = 0       // Major version component of the current release
-	Minor = 6       // Minor version component of the current release
+	Minor = 7       // Minor version component of the current release
 	Patch = 0       // Patch version component of the current release
 	Meta  = "alpha" // Version metadata to append to the version string
 )
